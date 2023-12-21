8a9f4a251b2098b5e6cf10d3b6443037dfc03806
danielterziev92 <daniel.st.terziev@gmail.com>
2023-12-21 22:01:32 +0200

    Change test_user_app tests

--- tests/users_app/models/test_user_app.py
+++ tests/users_app/models/test_user_app.py
@@ -1,4 +1,4 @@
-from django.db import IntegrityError
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
 from lekipohodivplaninata.users_app.models import UserApp
@@ -10,34 +10,43 @@ class TestUserApp(TestCase):
         'password': 'password',
     }
 
-    def setUp(self):
-        self.test_user = UserApp.objects.create(
-            email=self.VALID_USER_APP_DATA['email'],
-            password=self.VALID_USER_APP_DATA['password'],
-        )
+    def _create_user(self, data, **kwargs):
+        return UserApp(**data, **kwargs)
 
     def test_create__when_valid_data__expect_to_be_created(self):
-        user = self.test_user
+        user = self._create_user(self.VALID_USER_APP_DATA)
+        user.full_clean()
+        user.save()
 
         self.assertIsNotNone(user.pk)
 
     def test_crate__when_already_exists__expect_to_raise_exception(self):
-        with self.assertRaises(IntegrityError):
-            UserApp.objects.create(
-                email=self.VALID_USER_APP_DATA['email'],
-                password=self.VALID_USER_APP_DATA['password'],
-            )
+        with self.assertRaises(ValidationError):
+            user1 = self._create_user(self.VALID_USER_APP_DATA)
+            user1.full_clean()
+            user1.save()
+            user2 = self._create_user(self.VALID_USER_APP_DATA)
+            user2.full_clean()
+            user2.save()
 
     def test_create__when_email_is_none__expect_to_raise_exception(self):
-        with self.assertRaises(IntegrityError):
-            UserApp.objects.create(
-                email=None,
-                password=self.VALID_USER_APP_DATA['password'],
+        with self.assertRaises(ValidationError):
+            user = self._create_user(
+                {
+                    'password': self.VALID_USER_APP_DATA['password'],
+                    'email': None
+                }
             )
+            user.full_clean()
+            user.save()
 
     def test_create__when_password_is_none__expect_to_raise_exception(self):
-        with self.assertRaises(IntegrityError):
-            UserApp.objects.create(
-                email=self.VALID_USER_APP_DATA['email'],
-                password=None,
+        with self.assertRaises(ValidationError):
+            user = self._create_user(
+                {
+                    'email': self.VALID_USER_APP_DATA['email'],
+                    'password': None
+                }
             )
+            user.full_clean()
+            user.save()
