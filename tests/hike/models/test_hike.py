a339a0cf9d28b25389f1ca337beb8cc59e0aebae
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-03 21:47:27 +0200

    Create happy case when test hike model

--- tests/hike/models/test_hike.py
+++ tests/hike/models/test_hike.py
@@ -1,29 +1,44 @@
+from datetime import datetime
+
+from django.core.exceptions import ValidationError
+from django.db import DataError
 from django.test import TestCase
 
-from lekipohodivplaninata.hike.models import Hike
+from lekipohodivplaninata.hike.models import Hike, HikeLevel, HikeType
 
 
 class TestHikeModel(TestCase):
+    VALID_HIKE_TYPE_DATA = {
+        'title': 'Test Type',
+    }
+
+    VALID_HIKE_LEVEL_DATA = {
+        'title': 'Test Level',
+    }
+
     VALID_HIKE_DATA = {
         'title': 'Test',
         'slug': 'test',
-        'type': None,
         'description': 'Test description',
-        'level': None,
         'duration': '10 minutes',
-        'event_date': '2020-01-10',
-        'price': 10.10,
+        'event_date': datetime.now(),
+        'price': '1234.56',
         'main_picture': '',
     }
 
     def _create_and_save_hike(self, data):
-        hike = Hike.objects.create(**data)
+        hike_type = HikeType.objects.create(**self.VALID_HIKE_TYPE_DATA)
+        hike_level = HikeLevel.objects.create(**self.VALID_HIKE_LEVEL_DATA)
+
+        hike = Hike.objects.create(type=hike_type, level=hike_level, **data)
         hike.full_clean()
         hike.save()
         return hike
 
     def test_create_hike__when_valid_data__expect_to_be_created(self):
-        pass
+        hike = self._create_and_save_hike(self.VALID_HIKE_DATA)
+        self.assertEqual(Hike.objects.count(), 1)
+        self.assertEqual(hike.title, self.VALID_HIKE_DATA['title'])
 
     def test_create_hike__when_title_is_greater_with_one_character__expect_to_raise_exception(self):
         pass
