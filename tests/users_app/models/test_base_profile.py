14694a3a570f3bb25b07ae96d3e12763754c1bc3
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-03 15:40:17 +0200

    Modify the tests for base profile model

--- tests/users_app/models/test_base_profile.py
+++ tests/users_app/models/test_base_profile.py
@@ -24,25 +24,26 @@ class TestBaseProfile(TestCase):
         return user
 
     def test_create__when_valid_data__expect_to_be_created(self):
-        user = self._create_and_save_base_profile(self.VALID_BASE_PROFILE_DATA)
+        self._create_and_save_base_profile(self.VALID_BASE_PROFILE_DATA)
+        self.assertEqual(BaseProfile.objects.count(), 1)
 
     def test_create__when_first_name_with_one_more_character__expect_to_raise_exception(self):
         first_name = 'T' * BaseProfile.FIRST_NAME_MAX_LENGTH + 'T'
         with self.assertRaises(ValidationError):
             user_data = {**self.VALID_BASE_PROFILE_DATA, 'first_name': first_name}
-            user = self._create_and_save_base_profile(user_data)
+            self._create_and_save_base_profile(user_data)
 
     def test_create__when_last_name_with_one_more_character__expect_to_raise_exception(self):
         last_name = 'T' * BaseProfile.LAST_NAME_MAX_LENGTH + 'T'
         with self.assertRaises(ValidationError):
             user_data = {**self.VALID_BASE_PROFILE_DATA, 'last_name': last_name}
-            user = self._create_and_save_base_profile(user_data)
+            self._create_and_save_base_profile(user_data)
 
     def test_create__when_phone_number_with_one_more_character__expect_to_raise_exception(self):
         phone_number = '1' * BaseProfile.PHONE_NUMBER_MAX_LENGTH + '1'
         with self.assertRaises(ValidationError):
             user_data = {**self.VALID_BASE_PROFILE_DATA, 'phone_number': phone_number}
-            user = self._create_and_save_base_profile(user_data)
+            self._create_and_save_base_profile(user_data)
 
     def test_first_name_prop__when_create_user_with_valid_data__expect_to_return_first_name(self):
         user = self._create_and_save_base_profile(self.VALID_BASE_PROFILE_DATA)
