0a7331aac2b89404c295d2e46044c5dcfb292093
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-04 14:17:59 +0200

    Add tests for assessment and comment of SiteEvaluation model

--- tests/base/models/test_evaluation.py
+++ tests/base/models/test_evaluation.py
@@ -1,39 +1,70 @@
+from datetime import datetime
+
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from lekipohodivplaninata.base.models import Evaluation
+from lekipohodivplaninata.base.models import SiteEvaluation
 
 
-class TestEvaluationModel(TestCase):
-    VALID_EVALUATION_DATA = {
+class TestSiteEvaluationModel(TestCase):
+    VALID_SITE_EVALUATION_DATA = {
         'assessment': 5,
         'comment': 'Test Comment',
+        'rated_in': datetime.now()
     }
 
     def _create_and_save_evaluation(self, data):
-        evaluation = Evaluation.objects.create(**data)
-        evaluation.full_clean()
-        evaluation.save()
-        return evaluation
+        site_evaluation = SiteEvaluation.objects.create(**data)
+        site_evaluation.full_clean()
+        site_evaluation.save()
+        return site_evaluation
 
-    def test_create_evaluation__when_valida_data__expect_to_be_created(self):
-        evaluation = self._create_and_save_evaluation(self.VALID_EVALUATION_DATA)
-        self.assertEqual(Evaluation.objects.count(), 1)
-        self.assertEqual(evaluation.assessment, self.VALID_EVALUATION_DATA['assessment'])
+    def test_create_site_evaluation__when_valida_data__expect_to_be_created(self):
+        site_evaluation = self._create_and_save_evaluation(self.VALID_SITE_EVALUATION_DATA)
+        self.assertEqual(SiteEvaluation.objects.count(), 1)
+        self.assertEqual(site_evaluation.assessment, self.VALID_SITE_EVALUATION_DATA['assessment'])
 
-    def test_create_evaluation__when_assessment_is_smaller_then_min_value__expect_to_raise_exception(self):
-        pass
+    def test_create_site_evaluation__when_assessment_is_smaller_then_min_value__expect_to_raise_exception(self):
+        assessment = SiteEvaluation.ASSESSMENT_MIN_VALUE - 1
+        with self.assertRaises(ValidationError):
+            site_evaluation_data = {**self.VALID_SITE_EVALUATION_DATA, 'assessment': assessment}
+            self._create_and_save_evaluation(site_evaluation_data)
 
-    def test_create_evaluation__when_assessment_is_equal_to_min_value__expect_to_be_created(self):
-        pass
+    def test_create_site_evaluation__when_assessment_is_equal_to_min_value__expect_to_be_created(self):
+        assessment = SiteEvaluation.ASSESSMENT_MIN_VALUE
+        site_evaluation_data = {**self.VALID_SITE_EVALUATION_DATA, 'assessment': assessment}
 
-    def test_create_evaluation__when_assessment_is_bigger_then_max_value__expect_to_raise_exception(self):
-        pass
+        site_evaluation = self._create_and_save_evaluation(site_evaluation_data)
 
-    def test_create_evaluation__when_assessment_is_equal_to_max_value__expect_to_be_created(self):
-        pass
+        self.assertEqual(SiteEvaluation.objects.count(), 1)
+        self.assertEqual(site_evaluation.assessment, assessment)
 
-    def test_create_evaluation__when_assessment_is_null__expect_to_be_created(self):
-        pass
+    def test_create_site_evaluation__when_assessment_is_bigger_then_max_value__expect_to_raise_exception(self):
+        assessment = SiteEvaluation.ASSESSMENT_MAX_VALUE + 1
+        with self.assertRaises(ValidationError):
+            site_evaluation_data = {**self.VALID_SITE_EVALUATION_DATA, 'assessment': assessment}
+            self._create_and_save_evaluation(site_evaluation_data)
 
-    def test_create_evaluation__when_comment_is_null__expect_to_be_created(self):
-        pass
+    def test_create_site_evaluation__when_assessment_is_equal_to_max_value__expect_to_be_created(self):
+        assessment = SiteEvaluation.ASSESSMENT_MAX_VALUE
+        site_evaluation_data = {**self.VALID_SITE_EVALUATION_DATA, 'assessment': assessment}
+
+        site_evaluation = self._create_and_save_evaluation(site_evaluation_data)
+
+        self.assertEqual(SiteEvaluation.objects.count(), 1)
+        self.assertEqual(site_evaluation.assessment, assessment)
+
+    def test_create_site_evaluation__when_assessment_is_null__expect_to_be_created(self):
+        assessment = None
+        with self.assertRaises(ValidationError):
+            site_evaluation_data = {**self.VALID_SITE_EVALUATION_DATA, 'assessment': assessment}
+            self._create_and_save_evaluation(site_evaluation_data)
+
+    def test_create_site_evaluation__when_comment_is_null__expect_to_be_created(self):
+        comment = None
+        site_evaluation_data = {**self.VALID_SITE_EVALUATION_DATA, 'comment': comment}
+
+        site_evaluation = self._create_and_save_evaluation(site_evaluation_data)
+
+        self.assertEqual(SiteEvaluation.objects.count(), 1)
+        self.assertEqual(site_evaluation.comment, comment)
