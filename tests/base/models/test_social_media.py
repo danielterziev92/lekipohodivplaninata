b0c518a91315419ce587d902781661a2f90fb305
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-05 15:00:03 +0200

    Fill happy case with valid data for social media model

--- tests/base/models/test_social_media.py
+++ tests/base/models/test_social_media.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from lekipohodivplaninata.base.models import SocialMedia
+
 
 class TestSocialMediaModel(TestCase):
     VALID_SOCIAL_MEDIA_DATA = {
@@ -10,10 +12,16 @@ class TestSocialMediaModel(TestCase):
     }
 
     def _create_and_save_social_media(self, data):
-        pass
+        social_media = SocialMedia.objects.create(**data)
+        social_media.full_clean()
+        social_media.save()
+        return social_media
 
     def test_create_social_media__when_valid_data__expect_to_be_created(self):
-        pass
+        social_media = self._create_and_save_social_media(self.VALID_SOCIAL_MEDIA_DATA)
+
+        self.assertEqual(SocialMedia.objects.count(), 1)
+        self.assertEqual(social_media.name, self.VALID_SOCIAL_MEDIA_DATA['name'])
 
     def test_create_social_media__when_name_is_with_one_more_character_more__expect_to_raise_exception(self):
         pass
