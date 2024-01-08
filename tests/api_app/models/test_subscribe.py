f36006ad5832c8eb24b980450ae530755303dda9
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-08 23:35:09 +0200

    Add to all test with double model create that the second one is not created

--- tests/api_app/models/test_subscribe.py
+++ tests/api_app/models/test_subscribe.py
@@ -42,6 +42,8 @@ class TestSubscribeModel(TestCase):
             subscribe_data = {**self.VALID_SUBSCRIBE_DATA, 'slug_to_unsubscribe': 'test-slug-2'}
             self._create_and_save_subscribe(subscribe_data)
 
+            mock_send_email.assert_not_called()
+
     @patch('lekipohodivplaninata.core.tasks.send_email_to_subscriber.delay')
     def test_create__when_email_is_null__expect_to_raise_exception(self, mock_send_email):
         with self.assertRaises(IntegrityError):
@@ -60,5 +62,4 @@ class TestSubscribeModel(TestCase):
             subscribe_data = {**self.VALID_SUBSCRIBE_DATA, 'email': 'email2@example.com'}
             self._create_and_save_subscribe(subscribe_data)
 
-    def test_create__when_slug_is_null__expect_to_be_created(self):
-        pass
+            mock_send_email.assert_not_called()
