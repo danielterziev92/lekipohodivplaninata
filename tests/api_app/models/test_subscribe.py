2255a7032c909c1fdeb5b2fe65eabb078ced0d07
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-08 23:23:21 +0200

    Create happy case of test for subscribe model

--- tests/api_app/models/test_subscribe.py
+++ tests/api_app/models/test_subscribe.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
 from lekipohodivplaninata.api_app.models import Subscribe
@@ -7,7 +9,7 @@ class TestSubscribeModel(TestCase):
     VALID_SUBSCRIBE_DATA = {
         'email': 'email@example.com',
         'is_active': True,
-        'slug_to_unsubscribe': 'kvUt+d84hMcN@"}2T-:DL9',
+        'slug_to_unsubscribe': 'test-slug',
     }
 
     def _create_and_save_subscribe(self, data):
@@ -16,8 +18,17 @@ class TestSubscribeModel(TestCase):
         subscribe.save()
         return subscribe
 
-    def test_create__when_valid_data__expect_to_be_created(self):
-        pass
+    @patch('lekipohodivplaninata.core.tasks.send_email_to_subscriber.delay')
+    def test_create__when_valid_data__expect_to_be_created(self, mock_send_email):
+        subscribe = self._create_and_save_subscribe(self.VALID_SUBSCRIBE_DATA)
+
+        self.assertEqual(Subscribe.objects.count(), 1)
+        self.assertEqual(subscribe.email, self.VALID_SUBSCRIBE_DATA['email'])
+
+        # Test that subscribed_at is automatically set
+        self.assertIsNotNone(subscribe.subscribed_at)
+
+        mock_send_email.assert_called_once_with(email=subscribe.email)
 
     def test_create__when_email_is_not_unique__expect_to_raise_exception(self):
         pass
