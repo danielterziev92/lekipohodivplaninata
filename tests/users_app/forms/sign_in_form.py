08cb3093c157d0f1aecb7f151549e94901d65806
danielterziev92 <daniel.st.terziev@gmail.com>
2024-01-15 16:30:14 +0200

    Fill happy case to test SignInForm

--- tests/users_app/forms/sign_in_form.py
+++ tests/users_app/forms/sign_in_form.py
@@ -1,20 +1,27 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from lekipohodivplaninata.users_app.forms import SignInForm
+
 UserModel = get_user_model()
 
 
 class SignInFormTest(TestCase):
     VALID_DATA = {
-        'email': 'test@example.com',
+        'username': 'test@example.com',
         'password': 'Password123.',
     }
 
     def setUp(self):
-        self.user = UserModel.objects.create_user(**self.VALID_DATA)
+        user_data = {**self.VALID_DATA, 'email': self.VALID_DATA['username']}
+        del user_data['username']
+        self.user = UserModel.objects.create_user(**user_data)
 
     def test_form__when_valid_data__expect_to_be_submitted(self):
-        pass
+        form = SignInForm(data=self.VALID_DATA)
+        is_valid = form.is_valid()
+
+        self.assertTrue(is_valid)
 
     def test_form__when_email_is_not_email__expect_to_return_message(self):
         pass
